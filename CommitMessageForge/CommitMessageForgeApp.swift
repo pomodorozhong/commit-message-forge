7ff37f3a1659dcbbe0a1f1d4ad3f19cc6c46975b
pomodorozhong <pomodorozhong@gmail.com>
2022-05-04 10:30:14 +0800

    Initial Commit

---
+++ CommitMessageForge/CommitMessageForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitMessageForgeApp.swift
+//  CommitMessageForge
+//
+//  Created by Pomodoro Zhong on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct CommitMessageForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
