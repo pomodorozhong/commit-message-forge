319f278f06fc0491a1d06381ee2678e0e342cf9f
pomodorozhong <pomodorozhong@gmail.com>
2022-05-05 14:35:20 +0800

    feat✨(view): add `EditorView`

---
+++ CommitMessageForge/EditorView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct EditorView: View {
+
+  var body: some View {
+    Text("Hello, world!")
+  }
+}
+
+extension EditorView {
+
+}
+
+struct EditorView_Previews: PreviewProvider {
+  static var previews: some View {
+    EditorView()
+  }
+}
